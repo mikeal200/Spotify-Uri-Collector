ff4457b82bc6af7d57f183a1d4279dc33c63febb
Michael Williams <willi6mc@mail.uc.edu>
2020-03-24 19:58:11 -0400

    Updates

---
+++ precache-manifest.aad3efd17f46985c12aecb4e544717c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bdbb58bff052c914ce92f5be099262",
+    "url": "/Spotify-Uri-Collector/index.html"
+  },
+  {
+    "revision": "0aba5a47ba2f00736d41",
+    "url": "/Spotify-Uri-Collector/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "13ccdd53de406548c745",
+    "url": "/Spotify-Uri-Collector/static/js/2.08c7981a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spotify-Uri-Collector/static/js/2.08c7981a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aba5a47ba2f00736d41",
+    "url": "/Spotify-Uri-Collector/static/js/main.ff300d09.chunk.js"
+  },
+  {
+    "revision": "7780a6519267680e703e",
+    "url": "/Spotify-Uri-Collector/static/js/runtime-main.c1887c9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Spotify-Uri-Collector/static/media/logo.5d5d9eef.svg"
+  }
+]);
